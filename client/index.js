289b3ba0cc23b6fd0613b0e78ec4688908123649
hongdao <hongru-li@outlook.com>
2024-06-17 22:17:56 +0200

    completed architecht

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
-// set the dimensions and margins for the graphs
+// Set the dimensions and margins for the graphs
 const margin = { top: 20, right: 20, bottom: 30, left: 40 },
   width = 650 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
-// Hier folgt der fast fertig implementierte Code für die Erstellung des Balkendiagramms
+// Create scales for the bar chart
 let x = d3.scaleBand().range([0, width]).padding(0.1);
 let y = d3.scaleLinear().range([height, 0]);
 
+// Create the bar chart container
 const barchart = d3
   .select("#barchart")
   .append("svg")
@@ -15,122 +16,141 @@ const barchart = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-fetch("https://wetter-api.funkhaus-franken.de/niederschlag_monat.json")
+// Fetch data for the bar chart
+fetch("http://localhost:3000/wetterdaten/niederschlag")
   .then((response) => response.json())
   .then((json) => {
-    // TODO: Hier müssen die Daten von der API, welche in der json-Variable liegen, weiterverarbeitet werden
-    console.log(json);
+    const data = [];
+    const monthlyRain = {};
 
-    // TODO: Statt der folgenden Beispiel-Daten sollen die tatsächlichen Daten vom Server im return übergeben werden. Achtung: Das Format muss identisch sein!
-    const exampleData = [
-      { month: "Januar", rain: 45.3 },
-      { month: "Februar", rain: 25.3 },
-      { month: "März", rain: 0.5 },
-      { month: "April", rain: 12.9 },
-      { month: "Mai", rain: 54.3 },
-      { month: "Juni", rain: 44.5 },
-      { month: "Juli", rain: 34.4 },
-      { month: "August", rain: 13.3 },
-      { month: "September", rain: 41.5 },
-      { month: "Oktober", rain: 77.1 },
-      { month: "November", rain: 22.3 },
-      { month: "Dezember", rain: 43.9 },
-    ];
+    for (let key in json) {
+      const month = key.slice(4, 6);
+      const rain = parseFloat(json[key].MO_RR);
+      if (!monthlyRain[month]) monthlyRain[month] = { total: 0, count: 0 };
+      if (rain !== -999) {
+        monthlyRain[month].total += rain;
+        monthlyRain[month].count += 1;
+      }
+    }
 
-    return exampleData;
+    for (let month in monthlyRain) {
+      const avgRain = monthlyRain[month].total / monthlyRain[month].count;
+      data.push({ month, rain: avgRain });
+    }
+
+    // Sort data by month
+    const monthOrder = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    data.sort((a, b) => monthOrder.indexOf(a.month) - monthOrder.indexOf(b.month));
+
+    // Map month number to names
+    const monthNames = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    data.forEach(d => d.month = monthNames[parseInt(d.month) - 1]);
+
+    return data;
   })
   .then((data) => {
-    // scale the range of the data in the domains
-    x.domain(
-      data.map(function (d) {
-        return d.month;
-      })
-    );
-    y.domain([
-      0,
-      d3.max(data, function (d) {
-        return d.rain;
-      }),
-    ]);
+    // Scale the range of the data in the domains
+    x.domain(data.map(d => d.month));
+    y.domain([0, d3.max(data, d => d.rain)]);
 
-    // append the rectangles for the bar chart
-    barchart
-      .selectAll(".bar")
+    // Append the rectangles for the bar chart
+    barchart.selectAll(".bar")
       .data(data)
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", function (d) {
-        return x(d.month);
-      })
+      .attr("x", d => x(d.month))
       .attr("width", x.bandwidth())
-      .attr("y", function (d) {
-        return y(d.rain);
-      })
-      .attr("height", function (d) {
-        return height - y(d.rain);
-      });
+      .attr("y", d => y(d.rain))
+      .attr("height", d => height - y(d.rain));
 
-    // add the x axis with labels
-    barchart
-      .append("g")
+    // Add the x axis with labels
+    barchart.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-    // add the y axis with labels
-    barchart.append("g").call(d3.axisLeft(y));
+    // Add the y axis with labels
+    barchart.append("g")
+      .call(d3.axisLeft(y));
+
+    // Identify the summer month with the least rain
+    const summerMonths = data.filter(d => ["Juni", "Juli", "August", "September"].includes(d.month));
+    const minRainMonth = summerMonths.reduce((min, d) => d.rain < min.rain ? d : min, summerMonths[0]);
+
+    return minRainMonth.month;
+  })
+  .then((minRainMonth) => {
+    fetch("http://localhost:3000/wetterdaten/temperatur")
+      .then(response => response.json())
+      .then((json) => {
+        const monthIndex = {
+          "Juni": "06",
+          "Juli": "07",
+          "August": "08",
+          "September": "09"
+        }[minRainMonth];
+
+        const temperatureAccum = {};
+        for (let key in json) {
+          if (key.slice(4, 6) === monthIndex && key.slice(8, 10) === "18") {
+            const day = key.slice(6, 8);
+            const temp = parseFloat(json[key].TT_TER);
+            if (!temperatureAccum[day]) temperatureAccum[day] = { total: 0, count: 0 };
+            temperatureAccum[day].total += temp;
+            temperatureAccum[day].count += 1;
+          }
+        };
+
+        const temperatureData = Object.keys(temperatureAccum).map(day => {
+          return { day: parseInt(day), temperature: temperatureAccum[day].total / temperatureAccum[day].count };
+        });
+
+        return temperatureData;
+      })
+      .then(data => {
+        // Sort the tata by day
+        data.sort((a, b) => a.day - b.day);
+
+        const linechart = d3
+          .select("#linechart")
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const xLine = d3
+          .scaleLinear()
+          .domain([1, d3.max(data, (d) => d.day)])
+          .range([0, width]);
+
+        const yLine = d3
+          .scaleLinear()
+          .domain([d3.min(data, d => d.temperature), d3.max(data, (d) => d.temperature)])
+          .range([height, 0]);
+
+        // Add the X Axis
+        linechart
+          .append("g")
+          .attr("transform", `translate(0,${height})`)
+          .call(d3.axisBottom(xLine));
+
+        // Add the Y Axis
+        linechart.append("g").call(d3.axisLeft(yLine));
+
+        // Create the line generator
+        const line = d3
+          .line()
+          .x((d) => xLine(d.day))
+          .y((d) => yLine(d.temperature));
+
+        // Add the line path
+        linechart
+          .append("path")
+          .datum(data)
+          .attr("fill", "none")
+          .attr("stroke", "steelblue")
+          .attr("stroke-width", 2)
+          .attr("d", line);
+      });
   });
-
-// TODO: Hier soll nun der Code für die Erstellung des Liniendiagramms folgen. Aktuell ist nur ein Beispiel-Diagramm implementiert, das als Ausgangspunkt dienen soll.
-// Data
-const data = [
-  { x: 1, y: 10 },
-  { x: 2, y: 20 },
-  { x: 3, y: 15 },
-  { x: 4, y: 25 },
-  { x: 5, y: 30 },
-];
-
-// Append the SVG object to the div called 'chart'
-const linechart = d3
-  .select("#linechart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
-
-// Create scales
-const xLine = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.x)])
-  .range([0, width]);
-
-const yLine = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.y)])
-  .range([height, 0]);
-
-// Add the X Axis
-linechart
-  .append("g")
-  .attr("transform", `translate(0,${height})`)
-  .call(d3.axisBottom(xLine));
-
-// Add the Y Axis
-linechart.append("g").call(d3.axisLeft(yLine));
-
-// Create the line generator
-const line = d3
-  .line()
-  .x((d) => xLine(d.x))
-  .y((d) => yLine(d.y));
-
-// Add the line path
-linechart
-  .append("path")
-  .datum(data)
-  .attr("fill", "none")
-  .attr("stroke", "steelblue")
-  .attr("stroke-width", 2)
-  .attr("d", line);
--- server/app.js
+++ server/app.js
@@ -1,12 +1,42 @@
 import http from "http";
 import express from "express";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
 
 const app = express();
 app.server = http.createServer(app);
+app.use(cors());
 
 // Adding a default route at /
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
+});
+
+// Route to serve niederschlag data
+app.get("/wetterdaten/niederschlag", (req, res) => {
+  const filePath = path.join("server", "niederschlag_monat.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+      return;
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.send(data);
+  });
+});
+
+// Route to serve temperatur data
+app.get("/wetterdaten/temperatur", (req, res) => {
+  const filePath = path.join("server", "temperatur_drei_mal_taeglich.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+      return;
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.send(data);
+  });
 });
 
 app.server.listen(3000, () => {
